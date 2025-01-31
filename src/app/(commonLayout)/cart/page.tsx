0df37fd767509755a9b232f78cabfe1b3caa7c6e
Rahul Sutradhar <rahulsd380@gmail.com>
2025-01-31 09:13:52 +0600

    Added proceed to pay card in cart page

--- src/app/(commonLayout)/cart/page.tsx
+++ src/app/(commonLayout)/cart/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import CartItems from "@/Components/Cart/CartItems/CartItems";
 import ProceedToPay from "@/Components/Cart/ProceedToPay/ProceedToPay";
 import Container from "@/Components/Shared/Container/Container";
@@ -5,16 +6,18 @@ import Container from "@/Components/Shared/Container/Container";
 const Cart = () => {
     return (
         <Container>
-            <div className="font-Inter">
+            <div className="font-Inter mt-36">
                 <h1 className="text-neutral-45 text-xl md:text-[36px] font-bold leading-normal">
-                    Programmes You Added
+                    Programmes You've Added
                 </h1>
                 <h2 className="text-neutral-45 text-2xl font-semibold leading-9 mt-[14px]">
                     3 Items in your Cart
                 </h2>
 
-                <CartItems/>
-                <ProceedToPay/>
+                <div className="flex gap-6">
+                    <CartItems />
+                    <ProceedToPay />
+                </div>
             </div>
         </Container>
     );
