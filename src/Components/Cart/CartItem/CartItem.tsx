04622d17f309530fd7988de57f7be6bb0b81cb01
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-01 12:17:30 +0600

    Showing cart ityems on ui

--- src/Components/Cart/CartItem/CartItem.tsx
+++ src/Components/Cart/CartItem/CartItem.tsx
@@ -1,14 +1,16 @@
-import { ICONS, IMAGES } from "@/assets";
+import { ICONS } from "@/assets";
+import { useCart } from "@/providers/CartProvider/CartProvider";
 import Image from "next/image";
 
-const CartItems = () => {
+const CartItem = ({_id, name, image, price, duration, lessons}) => {
+    const { removeCourseFromCart } = useCart();
     return (
         <div className="flex flex-col gap-8 w-full lg:w-[70%]">
             <div className="flex items-center justify-between bg-white border-b border-neutral-100 pb-[22px]">
                 <div className="flex items-center gap-4">
-                    <Image src={IMAGES.backendDevelopmentImg} alt="lessons" className="rounded-lg w-[160px] h-[116px]" />
+                    <Image src={image} alt="lessons" className="rounded-lg w-[160px] h-[116px]" width={160} height={116} />
                     <div className="flex flex-col gap-2">
-                        <h1 className="text-neutral-45 font-semibold leading-[25px] w-full max-w-[377px]">Backend Development Using Node.js, MongoDB, and Express.js</h1>
+                        <h1 className="text-neutral-45 font-semibold leading-[25px] w-full max-w-[377px]">{name}</h1>
                         <div className="flex items-center">
                             <p className="text-neutral-25 text-[13px] font-medium">5</p>
                             <Image src={ICONS.rating} alt="rating" className="" />
@@ -16,22 +18,22 @@ const CartItems = () => {
                         <div className="flex items-center gap-6">
                             <div className="flex items-center gap-2">
                                 <Image src={ICONS.lessons} alt="lessons" className="size-5" />
-                                <p className="text-neutral-25 text-[13px] leading-5">24 Lessons</p>
+                                <p className="text-neutral-25 text-[13px] leading-5">{lessons} Lessons</p>
                             </div>
                             <div className="flex items-center gap-2">
                                 <Image src={ICONS.duration} alt="lessons" className="size-5" />
-                                <p className="text-neutral-25 text-[13px] leading-5">3 months</p>
+                                <p className="text-neutral-25 text-[13px] leading-5">{duration}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <h2 className="text-neutral-45 text-2xl font-semibold leading-9 mt-[14px]">
-                    ₹999
+                    ₹{price}
                 </h2>
-                <h1 className="text-rose-500 font-semibold leading-[25px]">Remove</h1>
+                <button onClick={() => removeCourseFromCart(_id)} className="text-rose-500 font-semibold leading-[25px]">Remove</h1>
             </div>
         </div>
     );
 };
 
-export default CartItems;
+export default CartItem;
--- src/app/(commonLayout)/cart/page.tsx
+++ src/app/(commonLayout)/cart/page.tsx
@@ -1,10 +1,14 @@
+"use client"
+import CartItem from "@/Components/Cart/CartItem/CartItem";
 /* eslint-disable react/no-unescaped-entities */
-import CartItems from "@/Components/Cart/CartItems/CartItems";
 import ProceedToPay from "@/Components/Cart/ProceedToPay/ProceedToPay";
 import InterestedTrainings from "@/Components/ProgrammeDetails/InterestedTrainings/InterestedTrainings";
 import Container from "@/Components/Shared/Container/Container";
+import { useCart } from "@/providers/CartProvider/CartProvider";
 
 const Cart = () => {
+     const { cartData } = useCart();
+     console.log(cartData);
     return (
         <Container>
             <div className="font-Inter mt-36">
@@ -16,7 +20,14 @@ const Cart = () => {
                 </h2>
 
                 <div className="flex gap-6 mt-8">
-                    <CartItems />
+                    <div className="flex flex-col gap-8 w-full lg:w-[70%]">
+
+                    {
+                        cartData?.map(data => 
+                            <CartItem key={data?._id} {...data} />
+                        )
+                    }
+                    </div>
                     <ProceedToPay />
                 </div>
                 <InterestedTrainings/>
