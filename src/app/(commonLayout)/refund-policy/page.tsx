7e55ed8e695828517243a12c1dab70a9ea535075
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-02 21:24:43 +0600

    Added refund policy page

---
+++ src/app/(commonLayout)/refund-policy/page.tsx
@@ -0,0 +1,20 @@
+import PrivacyPolicyHero from "@/Components/PrivacyPolicy/PrivacyPolicyHero/PrivacyPolicyHero";
+import Container from "@/Components/Shared/Container/Container";
+
+const RefundPolicy = () => {
+    return (
+        <div>
+            <PrivacyPolicyHero title="Refund Policy" />
+            <Container>
+                <div className="font-Inter w-full max-w-[1100px] mx-auto flex flex-col gap-8 mt-14">
+                    <p className="font-normal text-lg">
+                        At MITR Consultancy, we follow a <strong>No Refund Policy</strong> for our internship programs. Once payment is made, no cancellations or refunds will be processed. We encourage users to carefully review the program details before making a payment.
+                    </p>
+                    <p className="font-normal text-lg">For any queries, please contact us at <a href="mailto:mitrconsultancy0101@gmail.com" className="font-semibold text-primary-20">mitrconsultancy0101@gmail.com</a>.</p>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default RefundPolicy;
