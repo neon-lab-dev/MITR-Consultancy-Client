3e1635f783fdf8751db23d1d1e1c3ee0f34bd809
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-10 22:14:03 +0600

    Fetched programmes data in admin panel

--- src/app/admin/programmes/page.tsx
+++ src/app/admin/programmes/page.tsx
@@ -2,28 +2,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import Table from "@/Components/Reusable/Table/Table";
+import { useGetAllProgrammesQuery } from "@/redux/Features/InterhshipProgrammes/internshipProgrammesApi";
 
 const Programmes = () => {
+    const { data: allProgrammes } = useGetAllProgrammesQuery({});
     const columns = [
-        { key: "id", label: "ID" },
-        { key: "name", label: "Programme Name" },
-        { key: "enrolledStudents", label: "Enrolled Students" },
+        { key: "_id", label: "ID" },
+        { key: "courseName", label: "Programme Name" },
+        { key: "totalEnrolled", label: "Enrolled Students" },
         { key: "price", label: "Price" },
         { key: "duration", label: "Duration" },
     ];
-      
-    const data = [
-        { id: 1, name: "Web Development Bootcamp", enrolledStudents: 150, price: "₹999", duration: "3 months" },
-        { id: 2, name: "Data Science Masterclass", enrolledStudents: 120, price: "₹999", duration: "3 months" },
-    ];
-      
+
     const actions = [
         { label: "Edit", onClick: (row: any) => console.log("Edit", row) },
         { label: "Delete", onClick: (row: any) => console.log("Delete", row) },
     ];
     return (
         <div>
-             <Table columns={columns} data={data} actions={actions} />
+            <Table columns={columns} data={allProgrammes?.courses} actions={actions} />
         </div>
     );
 };
