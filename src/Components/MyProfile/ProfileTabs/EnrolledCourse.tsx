3515533b76278226c600c6638355087e4ce491f1
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-14 12:33:32 +0600

    Added loader in enrolled courses page

--- src/Components/MyProfile/ProfileTabs/EnrolledCourse.tsx
+++ src/Components/MyProfile/ProfileTabs/EnrolledCourse.tsx
@@ -1,24 +1,32 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import TrainingCard from "@/Components/InternshipProgrammes/StartTraining/TrainingCard";
+import ProgrammeCardLoader from "@/Components/Loaders/ProgrammeCardLoader";
 import { useGetMyPurchasedCoursesQuery } from "@/redux/Features/User/userApi";
 
 const EnrolledCourse = () => {
-    const { data: myPurchasedCourses } = useGetMyPurchasedCoursesQuery({});
+    const { data: myPurchasedCourses, isLoading } = useGetMyPurchasedCoursesQuery({});
 
     return (
         <div>
             {
-                myPurchasedCourses?.purchasedCourses?.length > 0 ?
+                isLoading ?
                     <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10 mt-9">
                         {
-                            myPurchasedCourses?.purchasedCourses?.map((course: any) => (
-                                <TrainingCard key={course?._id} isDescriptionVisible={true} isPriceVisible={false} imageHeight="h-[268px]" {...course} />
-                            ))
+                            [...Array(4)].map((_, index) => <ProgrammeCardLoader key={index} />)
                         }
                     </div>
                     :
-                    <p className="mt-5">Not enrolled in any course yet.</p>
+                    myPurchasedCourses?.purchasedCourses?.length > 0 ?
+                        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10 mt-9">
+                            {
+                                myPurchasedCourses?.purchasedCourses?.map((course: any) => (
+                                    <TrainingCard key={course?._id} isDescriptionVisible={true} isPriceVisible={false} imageHeight="h-[268px]" {...course} />
+                                ))
+                            }
+                        </div>
+                        :
+                        <p className="mt-5">Not enrolled in any course yet.</p>
             }
         </div>
     );
