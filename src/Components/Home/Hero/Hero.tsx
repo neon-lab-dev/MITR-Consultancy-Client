eaff2f1b3347c0b94e212678195dec7955589cb0
rahulsd380 <rahulsd380@gmail.com>
2025-07-10 13:16:48 +0600

    Updated hero section

--- src/Components/Home/Hero/Hero.tsx
+++ src/Components/Home/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { IMAGES } from "@/assets";
 import ContactUs from "@/Components/Home/ContactUs/ContactUs";
 import Button from "@/Components/Reusable/Button/Button";
@@ -6,50 +6,60 @@ import Container from "@/Components/Shared/Container/Container";
 import Image from "next/image";
 import { useState } from "react";
 
-
 const Hero = () => {
-    const [isContactUsModalOpen, setIsContactUsModalOpen] = useState<boolean>(false);
-    return (
-        <div className="font-Inter relative h-[600px] xl:h-screen">
-            {/* Background Image */}
-            <div className="absolute inset-0 z-0">
-                <Image
-                    src={IMAGES.heroImg}
-                    alt="MITRA Consultancy"
-                    layout="fill"
-                    objectFit="cover"
-                    priority
-                />
-            </div>
+  const [isContactUsModalOpen, setIsContactUsModalOpen] =
+    useState<boolean>(false);
+  return (
+    <div className="font-Inter relative h-[600px] xl:h-screen">
+      {/* Background Image */}
+      <div className="absolute inset-0 z-0">
+        <Image
+          src={IMAGES.heroImg}
+          alt="MITRA Consultancy"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
 
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-neutral-50 bg-opacity-50 z-10"></div>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-neutral-50 bg-opacity-50 z-10"></div>
 
-            {/* Content */}
-            <Container>
-                <div className="flex items-center xl:items-start justify-center absolute top-0 bottom-0 flex-col z-20 text-center xl:text-start">
-                    <h1 className="text-white leading-6 md:leading-[40px] xl:leading-[80px] text-xl md:text-[32px] xl:text-[68px] font-bold max-w-[344px] md:max-w-[550px] xl:max-w-[1071px]">
-                        Bring your thoughts to reality with{" "}
-                        <span className="text-primary-20">MITRA Consultancy</span>
-                    </h1>
-                    <p className="text-white text-[11px] md:text-sm xl:text-lg leading-[18px] xl:leading-7 max-w-[1071px] mt-3 xl:mt-6 px-4 2xl:px-0">
-                        Welcome to MITRA Consulting – where innovation and excellence converge. We provide tailored digital solutions designed to bring your ideas to life and drive your business success.
-                    </p>
-                    <div className="flex flex-col md:flex-row items-center justify-start gap-4 xl:gap-6 mt-4 md:mt-9 xl:mt-[62px]">
-                        <Button handleClick={() => setIsContactUsModalOpen(true)} variant="primary" title="Start a Project" classNames="w-[200px]" />
-                        <Button handleClick={() =>
+      {/* Content */}
+      <Container>
+        <div className="flex items-center xl:items-start justify-center absolute top-0 bottom-0 flex-col z-20 text-center xl:text-start">
+          <h1 className="text-white leading-7 md:leading-[40px] xl:leading-[73px] text-xl md:text-[32px] xl:text-[55px] font-bold max-w-[344px] md:max-w-[550px] xl:max-w-[1071px]">
+            All-in-One Digital Partner for Development, Design, Cybersecurity,
+            and Online Training –
+            <span className="text-primary-20"> MITRA Consultancy</span>
+          </h1>
+          <p className="text-white text-[11px] md:text-sm xl:text-base leading-[18px] xl:leading-7 max-w-full  xl:max-w-[1000px] mx-auto xl:mx-0 mt-3 xl:mt-6 px-4 2xl:px-0">
+            MITRA Consultancy offers expert web and app development, UI/UX
+            design, cybersecurity and compliance solutions, along with exclusive
+            3-month online training programs for students — your all-in-one
+            destination for building, securing, and growing in the digital
+            world.
+          </p>
+          <div className="flex flex-col md:flex-row items-center justify-start gap-4 xl:gap-6 mt-4 md:mt-9 xl:mt-[50px]">
+            <Button
+              handleClick={() => setIsContactUsModalOpen(true)}
+              variant="primary"
+              title="Talk to Us"
+              classNames="w-[200px]"
+            />
+            {/* <Button handleClick={() =>
                             document
                                 .getElementById("portfolio")
-                                ?.scrollIntoView({ behavior: "smooth" })} variant="secondary" title="View Our Works" classNames="w-[200px]" />
-                    </div>
-                </div>
-            </Container>
-            <ContactUs
-                isContactUsModalOpen={isContactUsModalOpen}
-                setIsContactUsModalOpen={setIsContactUsModalOpen}
-            />
+                                ?.scrollIntoView({ behavior: "smooth" })} variant="secondary" title="View Our Works" classNames="w-[200px]" /> */}
+          </div>
         </div>
-    );
+      </Container>
+      <ContactUs
+        isContactUsModalOpen={isContactUsModalOpen}
+        setIsContactUsModalOpen={setIsContactUsModalOpen}
+      />
+    </div>
+  );
 };
 
 export default Hero;
