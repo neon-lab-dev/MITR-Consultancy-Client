b42926556192b6603c86ac1b079fea1ccb403bec
prernabadwane <prernabadwane@gmail.com>
2025-08-06 16:59:52 +0530

    added proper images

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,29 +1,29 @@
-import AboutUs from "@/Components/Home/AboutUs/AboutUs";
-import Achievements from "@/Components/Home/Achievements/Achievements";
+// import AboutUs from "@/Components/Home/AboutUs/AboutUs";
+// import Achievements from "@/Components/Home/Achievements/Achievements";
 import FAQ from "@/Components/Shared/FAQ/FAQ";
-import Hero from "@/Components/Home/Hero/Hero";
+// import Hero from "@/Components/Home/Hero/Hero";
 import LetsTalk from "@/Components/Home/LetsTalk/LetsTalk";
-import OurServices from "@/Components/Home/OurServices/OurServices";
-import Technologies from "@/Components/Home/Technologies/Technologies";
-import Testimonials from "@/Components/Home/Testimonials/Testimonials";
-import OurProjects from './../../Components/Home/OurProjects/OurProjects';
-import AboutSecurityService from "@/Components/Home/AboutSecurityService/AboutSecurityService";
-import Counters from "@/Components/Home/Counters/Counters";
+// import OurServices from "@/Components/Home/OurServices/OurServices";
+// import Technologies from "@/Components/Home/Technologies/Technologies";
+// import Testimonials from "@/Components/Home/Testimonials/Testimonials";
+// import OurProjects from './../../Components/Home/OurProjects/OurProjects';
+// import AboutSecurityService from "@/Components/Home/AboutSecurityService/AboutSecurityService";
+// import Counters from "@/Components/Home/Counters/Counters";
 // import SessionAnnouncement from "@/Components/Shared/SessionAnnouncement/SessionAnnouncement";
 
 export default function Home() {
   return (
     <div >
-      <Hero/>
-      <Counters/>
-      <AboutUs/>
-      <OurServices/>
-      <Achievements/>
-      <AboutSecurityService/>
-      <OurProjects/>
-      <Technologies/>
+      {/* <Hero/> */}
+      {/* <Counters/> */}
+      {/* <AboutUs/> */}
+      {/* <OurServices/> */}
+      {/* <Achievements/> */}
+      {/* <AboutSecurityService/> */}
+      {/* <OurProjects/> */}
+      {/* <Technologies/> */}
       {/* <SessionAnnouncement/> */}
-      <Testimonials/>
+      {/* <Testimonials/> */}
       <FAQ/>
       <LetsTalk/>
     </div>
