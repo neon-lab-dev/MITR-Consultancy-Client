191937e2ae645d9e60d8747e5866ad34ede75604
rahulsd380 <rahulsd380@gmail.com>
2025-08-07 13:42:07 +0600

    Integrated emailjs

--- src/Components/CybersecurityCompliance/ComplianceAssessment/ComplianceAssessmentForm.tsx
+++ src/Components/CybersecurityCompliance/ComplianceAssessment/ComplianceAssessmentForm.tsx
@@ -1,14 +1,19 @@
-"use client"
-import { useForm } from "react-hook-form";
+"use client";
+import { useRef, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { toast } from "sonner";
+import Button2 from "@/Components/Reusable/Button/Button2";
+import LoadingSpinner from "@/Components/LoadingSpinner/LoadingSpinner";
 
-type ComplianceFormData = {
+type TComplianceFormData = {
   firstName: string;
   lastName: string;
   email: string;
   companyName: string;
-  challenge: string;
+  cybersecurityChallanges: string;
   location: string;
-  revenue: string;
+  annualRevenue: string;
   website: string;
 };
 
@@ -17,18 +22,48 @@ const ComplianceAssessmentForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ComplianceFormData>();
+    reset,
+  } = useForm<TComplianceFormData>();
 
-  const onSubmit = (data: ComplianceFormData) => {
-    console.log("Submitted Data:", data);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useRef<HTMLFormElement>(null);
+
+  // Send email function
+  const sendEmail: SubmitHandler<TComplianceFormData> = () => {
+    setIsLoading(true);
+    if (!form.current) return;
+
+    emailjs
+      .sendForm(
+        "service_phm6lbd",
+        "template_u0msfvh",
+        form.current,
+        "Cg5X16aA9m-EFkzTu"
+      )
+      .then(
+        () => {
+          toast("Thanks for your interest. We will contact with you soon!", {
+            style: {
+              padding: "10px",
+              borderRadius: "5px",
+            },
+            duration: 3000,
+          });
+          setIsLoading(false);
+          reset();
+        },
+        (error) => {
+          console.error(error);
+          toast.error("Failed! Try again.");
+        }
+      );
   };
 
   const baseInputClass =
     "w-full border rounded-md px-4 py-2 transition duration-300";
   const getInputClass = (hasError: boolean) =>
-    `${baseInputClass} ${
-      hasError ? "border-red-500" : "border-gray-300"
-    }`;
+    `${baseInputClass} ${hasError ? "border-red-500" : "border-gray-300"}`;
 
   return (
     <div className="bg-neutral-150 rounded-xl p-6 font-Inter shadow-[0px_0px_50px_-13px_rgba(0,0,0,0.20)] w-full xl:w-[60%]">
@@ -36,11 +71,17 @@ const ComplianceAssessmentForm = () => {
         Start Your Quick Compliance Check
       </h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 gap-4">
+      <form
+        ref={form}
+        onSubmit={handleSubmit(sendEmail)}
+        className="grid grid-cols-1 gap-4"
+      >
         {/* Name */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
-            <label className="block text-neutral-145 text-[15px] font-medium mb-1">Name</label>
+            <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+              Name
+            </label>
             <input
               type="text"
               placeholder="First"
@@ -60,7 +101,9 @@ const ComplianceAssessmentForm = () => {
 
         {/* Email */}
         <div>
-          <label className="block text-neutral-145 text-[15px] font-medium mb-1">Work Email</label>
+          <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+            Work Email
+          </label>
           <input
             type="email"
             placeholder="Example@companyname.com"
@@ -71,7 +114,9 @@ const ComplianceAssessmentForm = () => {
 
         {/* Company Name */}
         <div>
-          <label className="block text-neutral-145 text-[15px] font-medium mb-1">Company Name</label>
+          <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+            Company Name
+          </label>
           <input
             type="text"
             placeholder="Google, Microsoft, etc..."
@@ -82,28 +127,46 @@ const ComplianceAssessmentForm = () => {
 
         {/* Challenge & Location */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {/* Company Name */}
           <div>
-            <label className="block text-neutral-145 text-[15px] font-medium mb-1">Cybersecurity Challenge</label>
-            <select
-              {...register("challenge", { required: true })}
-              className={getInputClass(!!errors.challenge)}
-            >
-              <option value="">Select</option>
-              <option value="phishing">Phishing</option>
-              <option value="ransomware">Ransomware</option>
-              <option value="compliance">Compliance</option>
-            </select>
+            <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+              Cybersecurity Challenge
+            </label>
+            <input
+              type="text"
+              placeholder="Google, Microsoft, etc..."
+              {...register("cybersecurityChallanges", { required: true })}
+              className={getInputClass(!!errors.cybersecurityChallanges)}
+            />
           </div>
+
           <div>
-            <label className="block text-neutral-145 text-[15px] font-medium mb-1">Location</label>
+            <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+              Location
+            </label>
             <select
               {...register("location", { required: true })}
               className={getInputClass(!!errors.location)}
             >
-              <option value="">Select</option>
-              <option value="usa">India</option>
-              <option value="europe">USA</option>
-              <option value="asia">England</option>
+              <option value="" selected disabled>
+                Select
+              </option>
+              <option value="india">India</option>
+              <option value="usa_middleeast">USA, Middle East</option>
+              <option value="canada_germany_japan">
+                Canada, Germany, Japan
+              </option>
+              <option value="france_italy_uk_southkorea">
+                France, Italy, UK, South Korea
+              </option>
+              <option value="asean_scandinavia">ASEAN, Scandinavia</option>
+              <option value="australia_southafrica">
+                Australia, South Africa
+              </option>
+              <option value="turkey_latinamerica_brazil">
+                Turkey, Latin America, Brazil
+              </option>
+              <option value="others">Others</option>
             </select>
           </div>
         </div>
@@ -111,19 +174,24 @@ const ComplianceAssessmentForm = () => {
         {/* Revenue & Website */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
-            <label className="block text-neutral-145 text-[15px] font-medium mb-1">Annual Revenue</label>
+            <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+              Annual Revenue
+            </label>
             <select
-              {...register("revenue", { required: true })}
-              className={getInputClass(!!errors.revenue)}
+              {...register("annualRevenue", { required: true })}
+              className={getInputClass(!!errors.annualRevenue)}
             >
               <option value="">Select</option>
-              <option value="under1m">Under $1M</option>
-              <option value="1mto10m">$1M - $10M</option>
-              <option value="10mplus">Over $10M</option>
+              <option value="under1m">Less than 10CR</option>
+              <option value="1mto10m">10CR-50CR</option>
+              <option value="10mplus">51CR-100CR</option>
+              <option value="10mplus">More than 100CR</option>
             </select>
           </div>
           <div>
-            <label className="block text-neutral-145 text-[15px] font-medium mb-1">Website</label>
+            <label className="block text-neutral-145 text-[15px] font-medium mb-1">
+              Website
+            </label>
             <input
               type="text"
               placeholder="www.companyname.com"
@@ -133,13 +201,9 @@ const ComplianceAssessmentForm = () => {
           </div>
         </div>
 
-        {/* Submit Button */}
-        <button
-          type="submit"
-          className="mt-4 bg-primary-10 text-white w-full py-2 rounded-md font-medium"
-        >
-          Check Now
-        </button>
+        <Button2 variant="primary" title="">
+          {isLoading ? <LoadingSpinner fontSize="text-[15px]" /> : "Check Now"}
+        </Button2>
       </form>
     </div>
   );
