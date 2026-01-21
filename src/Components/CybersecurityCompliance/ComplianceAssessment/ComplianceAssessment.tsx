9d43899e61be86a2a9c2e48478be6214d146c74f
rahulsd380 <rahulsd380@gmail.com>
2026-01-21 11:14:15 +0600

    Refactored talk to us section

--- src/Components/CybersecurityCompliance/ComplianceAssessment/ComplianceAssessment.tsx
+++ src/Components/CybersecurityCompliance/ComplianceAssessment/ComplianceAssessment.tsx
@@ -1,21 +1,14 @@
-import { IMAGES } from "@/assets";
 import Container from "@/Components/Shared/Container/Container";
-import Image from "next/image";
 import ComplianceAssessmentForm from "./ComplianceAssessmentForm";
-import Heading from "@/Components/Reusable/Heading/Heading";
+import StyledHeading from "@/Components/Reusable/StyledHeading/StyledHeading";
 
 const ComplianceAssessment = () => {
   return (
-    <div id="complianceAssessment" className="font-Inter relative">
-      <Image
-        src={IMAGES.secureCompliance}
-        alt=""
-        className="w-full h-full absolute z-0"
-      />
+    <div id="complianceAssessment" className="font-Satoshi bg-neutral-185">
 
       <Container>
         <div className="relative py-[100px]">
-          <Heading align="center" heading="Talk To Us Today" />
+          <StyledHeading heading="Talk To Us" stylesHeading="Today" />
 
           <div className="flex items-center justify-between mt-[60px]">
             <ComplianceAssessmentForm />
