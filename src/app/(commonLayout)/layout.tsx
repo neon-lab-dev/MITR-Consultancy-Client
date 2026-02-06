30b02ca1a4c463fcb59f3c8cc63cda5ab6fa1f8d
rahulsd380 <rahulsd380@gmail.com>
2026-02-06 19:04:39 +0600

    Added smooth sxroll

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,13 +1,16 @@
 import Footer from "@/Components/Shared/Footer/Footer";
 import Navbar from "@/Components/Shared/Navbar/Navbar";
+import SmoothScroll from "@/Components/SmoothScroll";
 import { ReactNode } from "react";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
+      <SmoothScroll>
       <Navbar />
       {children}
       <Footer />
+      </SmoothScroll>
     </div>
   );
 };
